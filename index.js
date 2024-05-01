0590981ab2bdc451edd361d79385f6235364ed89
lime2 <lime2@github.com>
2024-05-01 00:07:14 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Set the "public" directory as the static directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route for the home page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
